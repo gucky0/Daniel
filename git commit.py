a8be1acd05cbce6433b9b1b66c922db6ba4abc71
Hafiz <hafiz071197@gmail.com>
2021-12-25 18:32:16 +0800

    updated VBA Lesson

--- git commit.py
+++ git commit.py
@@ -1,22 +1,28 @@
 import os
-repo = r"C:\Users\hariiz\AppData\Local\Programs\Python\Python39\Scripts\Daniel"
+repo = os.getcwd()
 
-commit_message = "Updated SQL Lesson"
+destination = "Daniel"
+
+commit_message = "Updated VBA Lesson"
 os.chdir(repo)
 
-line2 = "git init"
-line3 = "git status"
-line4 = "git add -A"
-line5 = f'git commit -m "{commit_message}"'
-##line6 = "git remote add origin https://github.com/gucky0/Daniel"
-line7 = "git remote -v"
-line8 = "git push -f origin main"
+lines = [f"cd {repo}",
+         "git init",
+         "git status",
+         "git remote remove origin",
+         f"git remote add origin https://github.com/gucky0/{destination}",
+         "git remote -v",
+         "git pull origin main",
+         "git add -all",
+         f'git commit -m "{commit_message}"',
+         "git show-ref",
+         "git push origin main"]
 
 def osu(line):
     os.system(line + " & pause")
 
-lines = [var for var in dir() if not var.startswith("__") and var not in ("os", "osu", "repo", "commit_message")]
+##lines = [var for var in dir() if not var.startswith("__") and var not in ("os", "osu", "repo", "commit_message")]
 
 for line in lines:
-    osu(locals()[line])
+    osu(line)
 
